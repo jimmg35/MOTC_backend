e7842db17059ce3c6097d254768589bcd97b3be9
MSI\Jim <jim60308@gmail.com>
2022-01-26 01:47:52 +0800

    fuck

--- build/migration/1643132537167-ffff.js
+++ build/migration/1643132537167-ffff.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.initial1642730008741 = void 0;
-class initial1642730008741 {
+exports.ffff1643132537167 = void 0;
+class ffff1643132537167 {
     constructor() {
-        this.name = 'initial1642730008741';
+        this.name = 'ffff1643132537167';
     }
     async up(queryRunner) {
         await queryRunner.query(`CREATE TABLE "user" ("userId" uuid NOT NULL DEFAULT uuid_generate_v4(), "username" character varying(20) NOT NULL, "password" character varying NOT NULL, "alias" character varying(20), "email" character varying NOT NULL, "phoneNumber" character varying, "createdDate" TIMESTAMP NOT NULL DEFAULT now(), "mailConfirmationToken" character varying(128) NOT NULL, "isActive" boolean NOT NULL DEFAULT false, CONSTRAINT "UQ_78a916df40e02a9deb1c4b75edb" UNIQUE ("username"), CONSTRAINT "PK_d72ea127f30e21753c9e229891e" PRIMARY KEY ("userId"))`);
@@ -24,4 +24,4 @@ class initial1642730008741 {
         await queryRunner.query(`DROP TABLE "user"`);
     }
 }
-exports.initial1642730008741 = initial1642730008741;
+exports.ffff1643132537167 = ffff1643132537167;
--- src/migration/1643132537167-ffff.ts
+++ src/migration/1643132537167-ffff.ts
@@ -1,11 +1,9 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.init1643097144200 = void 0;
-class init1643097144200 {
-    constructor() {
-        this.name = 'init1643097144200';
-    }
-    async up(queryRunner) {
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class ffff1643132537167 implements MigrationInterface {
+    name = 'ffff1643132537167'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "user" ("userId" uuid NOT NULL DEFAULT uuid_generate_v4(), "username" character varying(20) NOT NULL, "password" character varying NOT NULL, "alias" character varying(20), "email" character varying NOT NULL, "phoneNumber" character varying, "createdDate" TIMESTAMP NOT NULL DEFAULT now(), "mailConfirmationToken" character varying(128) NOT NULL, "isActive" boolean NOT NULL DEFAULT false, CONSTRAINT "UQ_78a916df40e02a9deb1c4b75edb" UNIQUE ("username"), CONSTRAINT "PK_d72ea127f30e21753c9e229891e" PRIMARY KEY ("userId"))`);
         await queryRunner.query(`CREATE TABLE "role" ("id" SERIAL NOT NULL, "roleName" character varying(50) NOT NULL, CONSTRAINT "PK_b36bcfe02fc8de3c57a8b2391c2" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "user_roles_role" ("userUserId" uuid NOT NULL, "roleId" integer NOT NULL, CONSTRAINT "PK_b38bd5c220511055a802bbce985" PRIMARY KEY ("userUserId", "roleId"))`);
@@ -14,7 +12,8 @@ class init1643097144200 {
         await queryRunner.query(`ALTER TABLE "user_roles_role" ADD CONSTRAINT "FK_0bd606ba8531dd93b457b8486d9" FOREIGN KEY ("userUserId") REFERENCES "user"("userId") ON DELETE CASCADE ON UPDATE CASCADE`);
         await queryRunner.query(`ALTER TABLE "user_roles_role" ADD CONSTRAINT "FK_4be2f7adf862634f5f803d246b8" FOREIGN KEY ("roleId") REFERENCES "role"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
-    async down(queryRunner) {
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "user_roles_role" DROP CONSTRAINT "FK_4be2f7adf862634f5f803d246b8"`);
         await queryRunner.query(`ALTER TABLE "user_roles_role" DROP CONSTRAINT "FK_0bd606ba8531dd93b457b8486d9"`);
         await queryRunner.query(`DROP INDEX "public"."IDX_4be2f7adf862634f5f803d246b"`);
@@ -23,5 +22,5 @@ class init1643097144200 {
         await queryRunner.query(`DROP TABLE "role"`);
         await queryRunner.query(`DROP TABLE "user"`);
     }
+
 }
-exports.init1643097144200 = init1643097144200;
